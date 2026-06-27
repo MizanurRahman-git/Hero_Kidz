a5f15a2a86dc043b4669e14f594b0b4fd6f95135
Mizanur Rahman <mdmizanurrahmanhridoy5@gmail.com>
2026-06-28 01:14:50 +0600

    first commit

---
+++ src/lib/dbConnect.js
@@ -0,0 +1,19 @@
+const uri = process.env.MONGO_URI;
+const dbName = process.env.DBNAME;
+
+const collections = {
+    PRODUCTS: "products",
+}
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+export const dbConnect = (cname) => {
+    return client.db(dbName).collection(cname)
+}
