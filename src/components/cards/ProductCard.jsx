5b1c53fc3c9fc1457f450ab1d71881554c71cf40
Mizanur Rahman <mdmizanurrahmanhridoy5@gmail.com>
2026-06-29 01:00:45 +0600

    added productcard

---
+++ src/components/cards/ProductCard.jsx
@@ -0,0 +1,43 @@
+"use client"
+
+import React from "react";
+import Image from "next/image"
+import { FaStar, FaShoppingCart } from "react-icons/fa"
+
+const ProductCard = ({product}) => {
+  const { title, image, price, ratings, reviews, sold } = product;
+  return (
+    <div className="card bg-base-100 shadow-md hover:shadow-xl transition-all duration-300">
+      <figure className="relative h-56 bg-base-200">
+        <Image src={image} alt={title} fill  />
+      </figure>
+
+      <div className="card-body">
+        <h2 className="card-title text-base line-clamp-2 min-h-12">
+          {title}
+        </h2>
+
+        <div className="flex items-center justify-between text-sm text-gray-500">
+          <div className="flex items-center gap-1">
+            <FaStar className="text-yellow-500" />
+            <span>{ratings}</span>
+            <span>({reviews})</span>
+          </div>
+
+          <span>{sold} Sold</span>
+        </div>
+
+        <div className="mt-2 flex items-center justify-between">
+          <span className="text-2xl font-bold text-primary">৳{price}</span>
+
+          <button className="btn btn-primary">
+            <FaShoppingCart />
+            Add To Cart
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProductCard;
--- src/components/home/Products.jsx
+++ src/components/home/Products.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import products from "@/data/toys.json";
+import ProductCard from "../cards/ProductCard";
 
 const Products = () => {
   return (
     <div>
       <h2 className="text-center text-4xl font-bold mb-10">Our Products</h2>
 
-      <div>
+      <div className="grid md:grid-cols-3 gap-5">
         {products.map((product) => (
-          <li key={product.title}>{product.title}</li>
+          <ProductCard key={product.title} product={product}></ProductCard>
         ))}
       </div>
     </div>
