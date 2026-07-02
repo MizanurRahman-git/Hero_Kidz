7ec49bf65bbd04669bdb9fef329743d7eeccb346
Mizanur Rahman <mdmizanurrahmanhridoy5@gmail.com>
2026-07-02 21:44:05 +0600

    remove some code

--- src/app/products/[id]/page.jsx
+++ src/app/products/[id]/page.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 const ProductDetails =async ({params}) => {
     const {id} = await params
     const product = await getSingleProduct(id)
-    console.log(product)
+    
     return (
         <div>
             
